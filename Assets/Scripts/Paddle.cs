b7d585e3655e7ecd9bf8340664a062eae7e7d3a9
Lucas Kotowski <64120951+comuwayit@users.noreply.github.com>
2021-02-11 19:50:44 -0800

    Add files via upload

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    public bool isPlayer1;
+    public float speed;
+    public Rigidbody2D rb;
+
+    private float movement;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPlayer1)
+        {
+            movement = Input.GetAxisRaw("Vertical");
+        }
+        else
+        {
+            movement = Input.GetAxisRaw("Vertical2");
+        }
+
+        rb.velocity = new Vector2(rb.velocity.x, movement * speed);
+    }
+
+    internal static float GetAxisRaw(string v)
+    {
+        throw new NotImplementedException();
+    }
+}
